f4bacf24449c5c311cbfd77d31f949421596063c
Christian Hansen <chansen@liveramp.com>
2019-01-08 18:41:49 -0800

    long packer

--- src/main/java/com/liveramp/hyperminhash/LongPacker.java
+++ src/main/java/com/liveramp/hyperminhash/LongPacker.java
@@ -3,12 +3,20 @@ package com.liveramp.hyperminhash;
 class LongPacker {
 
   static long pack(int numLeadingZeroes, long rBits, int r) {
-    long rightAligned = (numLeadingZeroes << r) | rBits;
-
+    return (numLeadingZeroes << r) | rBits;
   }
 
-  static long getRBits(long register, int q, int r) {
+  static long unpackMantissa(long register, int r) {
+    // Just clear the exponent bits + any unused bits if 2^q + r < 64
+    return register << (Long.SIZE - r) >>> (Long.SIZE - r);
+  }
 
+  static int unpackExponent(long register, int q, int r) {
+    final int numUnusedBits = Long.SIZE - r - (1 << q);
+    return Math.toIntExact(
+        // Clear unused bits, then shift the exponent down into the 2^q least significant bits.
+        (register << (numUnusedBits)) >>> (numUnusedBits + r)
+    );
   }
 
 }
