e47efc979c0ac0c7f040c991e52068261fbdb8a8
Christian Hansen <chansen@liveramp.com>
2019-01-09 20:07:31 -0800

    tests build, but do not pass

---
+++ test/main/java/com/liveramp/hyperminhash/MinHashTestSuite.java
@@ -0,0 +1,213 @@
+package com.liveramp.hyperminhash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import org.apache.commons.codec.binary.Hex;
+import org.junit.Assert;
+
+class MinHashTestSuite {
+
+  private static final String LETTER_BYTES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  private static double getError(long result, long expected) {
+    if (result == expected) {
+      return 0;
+    }
+
+    if (expected == 0) {
+      return result;
+    }
+
+    long delta = Math.abs(result - expected);
+    return delta / (double) expected;
+  }
+
+  static <Sketch extends IntersectionSketch<Sketch>> void testCardinality(
+      final Sketch sk,
+      final int maxUniqueElements,
+      final int minTestCardinality,
+      final Random random,
+      final double maxPctErr) {
+    final Map<String, Boolean> unique = new HashMap<>();
+    int assertionCheckpoint = minTestCardinality;
+    while (unique.size() < maxUniqueElements) {
+      String str = randomString(random);
+      sk.offer(str.getBytes());
+      unique.put(str, true);
+
+      if (unique.size() % assertionCheckpoint == 0) {
+        long exact = unique.size();
+        long res = sk.cardinality();
+        assertionCheckpoint *= 10;
+
+        double pctError = 100 * getError(res, exact);
+        Assert.assertTrue(pctError <= maxPctErr);
+      }
+    }
+
+  }
+
+  static <Sketch extends IntersectionSketch<Sketch>> void testUnion(
+      final Sketch sk1,
+      final Sketch sk2,
+      final SketchCombiner<Sketch> combiner,
+      final int elementsPerSketch,
+      final double maxPctError,
+      final Random random) {
+    final Set<String> unique = new HashSet<>(2 * elementsPerSketch);
+
+    for (int i = 1; i <= elementsPerSketch; i++) {
+      String str = randomString(random);
+      sk1.offer(str.getBytes());
+      unique.add(str);
+
+      str = randomString(random);
+      sk2.offer(str.getBytes());
+      unique.add(str);
+    }
+
+    final Sketch msk = combiner.union(sk1, sk2);
+    final long exact = unique.size();
+    final long res = msk.cardinality();
+
+    final double pctError = 100 * getError(res, exact);
+    Assert.assertTrue(pctError <= maxPctError);
+  }
+
+  static <Sketch extends IntersectionSketch<Sketch>> void testIntersection(
+      final Sketch emptySketch,
+      final SketchCombiner<Sketch> combiner,
+      final int overlapSlices,
+      final int elementsInLeftSketches,
+      final double maxPctError) {
+    for (int j = 1; j < overlapSlices; j++) {
+
+      final Sketch sk1 = emptySketch.deepCopy();
+      final Sketch sk2 = emptySketch.deepCopy();
+
+      final double frac = j / (double) overlapSlices;
+
+      for (int i = 0; i < elementsInLeftSketches; i++) {
+        final String str = i + "";
+        sk1.offer(str.getBytes());
+      }
+
+      for (int i = (int) (frac * elementsInLeftSketches); i < 2 * elementsInLeftSketches; i++) {
+        final String str = i + "";
+        sk2.offer(str.getBytes());
+      }
+
+      final long expected = (long) (elementsInLeftSketches - elementsInLeftSketches * frac);
+      final long result = combiner.intersectionCardinality(sk1, sk2);
+
+      final double pctError = 100 * getError(result, expected);
+      Assert.assertTrue(
+          String.format(
+              "Expected error ratio to be at most %s but found %f",
+              maxPctError,
+              pctError),
+          pctError <= maxPctError);
+    }
+  }
+
+  static <Sketch extends IntersectionSketch<Sketch>> void testIntersectLargeSetWithSmall(
+      final Sketch emptySketch,
+      final SketchCombiner<Sketch> combiner,
+      final int smallSetSize,
+      final int bigSetSize,
+      final double maxPctErr) {
+
+    final Sketch smallSketch = emptySketch.deepCopy();
+    for (int i = 1; i < smallSetSize; i++) {
+      smallSketch.offer((i + "").getBytes());
+    }
+
+    final Sketch bigSketch = emptySketch.deepCopy();
+    for (long i = 1; i <= bigSetSize; i++) {
+      bigSketch.offer((i + "").getBytes());
+    }
+
+    final double expectedJaccardIndex = smallSetSize / (double) bigSetSize;
+    final long expectedIntersection = (long) (expectedJaccardIndex * bigSetSize);
+    final long actualIntersection = combiner.intersectionCardinality(smallSketch, bigSketch);
+    final double pctError = 100 * getError(actualIntersection, expectedIntersection);
+
+    // HyperMinHash performance starts decreasing as jaccard index becomes < 1%. On a Jaccard index this small
+    // we should hope for <100% error.
+    Assert.assertTrue(
+        String.format(
+            "Percent error for a small jaccard index (%s) should be less than 100, but found %f",
+            expectedJaccardIndex, pctError),
+        pctError < maxPctErr
+    );
+  }
+
+  static <Sketch extends IntersectionSketch<Sketch>> void testMultiwayIntersection(
+      final Sketch emptySketch,
+      final SketchCombiner<Sketch> combiner,
+      final int initialSketchSize,
+      final int initialIntersectionSize,
+      final int numIter) {
+
+    long sketchSize = initialSketchSize;
+    long intersectionSize = initialIntersectionSize;
+    // union size gets bigger, jaccard gets smaller
+    for (int i = 0; i < numIter; i++) {
+      Sketch sk1 = emptySketch.deepCopy();
+      Sketch sk2 = emptySketch.deepCopy();
+      Sketch sk3 = emptySketch.deepCopy();
+      Sketch sk4 = emptySketch.deepCopy();
+
+      buildIntersectingSketches(sketchSize, intersectionSize, sk1, sk2, sk3, sk4);
+
+      long expectedIntersection = initialIntersectionSize;
+      long actualIntersection = combiner.intersectionCardinality(sk1, sk2, sk3);
+      double pctError = 100 * getError(actualIntersection, expectedIntersection);
+      Assert.assertTrue(
+          String.format("Expected pctError to be <2, found %f. Expected: %d, Actual: %d", pctError,
+              expectedIntersection, actualIntersection),
+          pctError <= 5
+      );
+
+      intersectionSize <<= 1;
+      sketchSize <<= 1;
+    }
+  }
+
+  // builds equally sized sketches which share numSharedElements items
+  private static <Sketch extends IntersectionSketch<Sketch>> void buildIntersectingSketches(
+      long sketchSize,
+      long numSharedElements,
+      Sketch... sketches) {
+    assert sketchSize >= numSharedElements;
+
+    final long numIter = ((sketchSize - numSharedElements) * sketches.length + numSharedElements);
+    for (int i = 0; i < numIter; i++) {
+      byte[] val = (i + "").getBytes();
+      if (i < numSharedElements) {
+        for (Sketch sketch : sketches) {
+          sketch.offer(val);
+        }
+      } else {
+        sketches[i % sketches.length].offer(val);
+      }
+    }
+  }
+
+  private static String randomString(final Random random) {
+    final int n = Math.abs(random.nextInt()) % 32;
+    byte[] b = new byte[n];
+    for (int i = 0; i < n; i++) {
+      b[i] = (byte) LETTER_BYTES.charAt(randPositiveInt(random) % LETTER_BYTES.length());
+    }
+    return Hex.encodeHexString(b);
+  }
+
+
+  private static int randPositiveInt(final Random random) {
+    return Math.abs(random.nextInt());
+  }
+}
--- test/main/java/com/liveramp/hyperminhash/TestBetaMinHash.java
+++ test/main/java/com/liveramp/hyperminhash/TestBetaMinHash.java
@@ -1,14 +1,11 @@
 package com.liveramp.hyperminhash;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
-import com.liveramp.hyperminhash.test.AbstractMinHashTest;
-import java.util.Random;
 import org.junit.Test;
 
-public class TestBetaMinHash extends AbstractMinHashTest {
+public class TestBetaMinHash {
 
   @Test
   public void testZeroCardinality() {
@@ -19,14 +16,18 @@ public class TestBetaMinHash extends AbstractMinHashTest {
   @Test
   public void testCardinality() {
     final BetaMinHash sk = new BetaMinHash();
-    super.testCardinality(
-        sk,
-        10_000_000,
-        10_000,
-        new Random(),
-        2.0
+    final int maxUniqueElements = 10_000_000;
+    final int minTestCardinality = 10_000;
+    final double pctErr = 2.0;
+    RandomTestRunner.runRandomizedTest(
+        3,
+        (random) -> MinHashTestSuite.testCardinality(
+            sk,
+            maxUniqueElements,
+            minTestCardinality,
+            random,
+            pctErr)
     );
-
   }
 
   @Test
@@ -35,105 +36,57 @@ public class TestBetaMinHash extends AbstractMinHashTest {
     final BetaMinHash sk2 = new BetaMinHash();
     final BetaMinHashCombiner combiner = BetaMinHashCombiner.getInstance();
     final int elementsPerSketch = 1_500_000;
-    final double pctError = 2.0;
-    super.testUnion(sk1, sk2, combiner, elementsPerSketch, pctError, new Random());
-  }
-
-  @Test
-  public void testIntersectionCardinality() {
-    final int iters = 20;
-    final int k = 1_000_000;
-    final double pctError = 5.0;
-    final BetaMinHashCombiner combiner = BetaMinHashCombiner.getInstance();
-
-    for (int j = 1; j < iters; j++) {
-
-      final BetaMinHash sk1 = new BetaMinHash();
-      final BetaMinHash sk2 = new BetaMinHash();
-
-      final double frac = j / (double) iters;
-
-      for (int i = 0; i < k; i++) {
-        final String str = i + "";
-        sk1.offer(str.getBytes());
-      }
-
-      for (int i = (int) (frac * k); i < 2 * k; i++) {
-        final String str = i + "";
-        sk2.offer(str.getBytes());
-      }
-
-      final long expected = (long) (k - k * frac);
-      final long result = combiner.intersectionCardinality(sk1, sk2);
-
-      final double pctError = 100 * getError(result, expected);
-      final double expectedPctError = 5.0;
-      assertTrue(
-          String.format("Expected error ratio to be at most %s but found %f", expectedPctError,
-              pctError),
-          pctError <= expectedPctError);
-    }
-  }
-
-  @Test
-  public void testIntersectLargeSetWithSmallSet() {
-    // Our other test uses string, which puts an upper bound on how big that test can be due to GC errors. With numbers
-    // we can estimate cardinalities on the order of billions without running out of memory, unlike strings.
-    final BetaMinHash smallSketch = new BetaMinHash();
-    final BetaMinHashCombiner combiner = BetaMinHashCombiner.getInstance();
-    final long smallSetSize = 100_000;
-    for (int i = 1; i < smallSetSize; i++) {
-      smallSketch.offer((i + "").getBytes());
-    }
-
-    final BetaMinHash bigSketch = new BetaMinHash();
-    final long bigSetSize = 100_000_000;
-    for (long i = 1; i <= bigSetSize; i++) {
-      bigSketch.offer((i + "").getBytes());
-    }
-
-    final double expectedJaccardIndex = smallSetSize / (double) bigSetSize;
-    final long expectedIntersection = (long) (expectedJaccardIndex * bigSetSize);
-    final long actualIntersection = combiner.intersectionCardinality(smallSketch, bigSketch);
-    final double pctError = 100 * getError(actualIntersection, expectedIntersection);
-
-    // HyperMinHash performance starts decreasing as jaccard index becomes < 1%. On a Jaccard index this small
-    // we should hope for <100% error.
-    assertTrue(
-        String.format(
-            "Percent error for a small jaccard index (%s) should be less than 100, but found %f",
-            expectedJaccardIndex, pctError),
-        pctError < 100
+    final double pctErr = 2.0;
+    RandomTestRunner.runRandomizedTest(
+        3,
+        (random) -> MinHashTestSuite
+            .testUnion(sk1, sk2, combiner, elementsPerSketch, pctErr, random)
     );
   }
 
   @Test
-  public void testManyWayIntersection() {
+  public void testIntersectionCardinality() {
+    final int overlapSlices = 20;
+    final int numElementsLeftSketch = 1_000_000;
+    final double pctError = 5.0;
+    MinHashTestSuite.testIntersection(
+        new BetaMinHash(),
+        BetaMinHashCombiner.getInstance(),
+        overlapSlices,
+        numElementsLeftSketch,
+        pctError
+    );
 
-    long intersectionSize = 3000;
-    long sketchSize = 10_000;
-    final BetaMinHashCombiner combiner = BetaMinHashCombiner.getInstance();
-    // union size gets bigger, jaccard gets smaller
-    for (int i = 0; i < 5; i++) {
-      BetaMinHash sk1 = new BetaMinHash();
-      BetaMinHash sk2 = new BetaMinHash();
-      BetaMinHash sk3 = new BetaMinHash();
-      BetaMinHash sk4 = new BetaMinHash();
+  }
 
-      buildIntersectingSketches(sketchSize, intersectionSize, sk1, sk2, sk3, sk4);
+  @Test
+  public void testIntersectLargeSetWithSmallSet() {
+    final int smallSetSize = 100_000;
+    final int bigSetSize = 100_000_000;
+    final double maxPctErr = 100;
+    MinHashTestSuite.testIntersectLargeSetWithSmall(
+        new BetaMinHash(),
+        BetaMinHashCombiner.getInstance(),
+        smallSetSize,
+        bigSetSize,
+        maxPctErr
+    );
+  }
 
-      long expectedIntersection = intersectionSize;
-      long actualIntersection = combiner.intersectionCardinality(sk1, sk2, sk3);
-      double pctError = 100 * getError(actualIntersection, expectedIntersection);
-      assertTrue(
-          String.format("Expected pctError to be <2, found %f. Expected: %d, Actual: %d", pctError,
-              expectedIntersection, actualIntersection),
-          pctError <= 5
-      );
+  @Test
+  public void testMultiwayIntersection() {
 
-      intersectionSize <<= 1;
-      sketchSize <<= 1;
-    }
+    final int initialIntersectionSize = 3000;
+    final int initialSketchSize = 10_000;
+    final int numIter = 5;
+    MinHashTestSuite.testMultiwayIntersection(
+        new BetaMinHash(),
+        BetaMinHashCombiner.getInstance(),
+        initialIntersectionSize,
+        initialSketchSize,
+        numIter
+    );
+
   }
 
   @Test
@@ -145,24 +98,6 @@ public class TestBetaMinHash extends AbstractMinHashTest {
 //    final BetaMinHash deSerialized = BetaMinHash.fromBytes(serialized);
 
 //    Assert.assertEquals(original, deSerialized);
-  }
-
-  // builds equally sized sketches which share numSharedElements items
-  private void buildIntersectingSketches(
-      long sketchSize, long numSharedElements, BetaMinHash... sketches) {
-    assert sketchSize >= numSharedElements;
-
-    for (int i = 0; i < ((sketchSize - numSharedElements) * sketches.length + numSharedElements);
-        i++) {
-      byte[] val = (i + "").getBytes();
-      if (i < numSharedElements) {
-        for (BetaMinHash sketch : sketches) {
-          sketch.offer(val);
-        }
-      } else {
-        sketches[i % sketches.length].offer(val);
-      }
-    }
   }
 
 
